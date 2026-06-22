591a0a0668894d51ff78dff6a5843f4075915b8a
krishna <krishnaserves01@gmail.com>
2026-06-22 18:36:56 +0545

    Initialized Project HouZing

---
+++ server/HouZing/src/main/java/com/krishna/project/HouZing/HouZingApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.project.HouZing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouZingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouZingApplication.class, args);
+	}
+
+}
---
+++ server/HouZing/src/test/java/com/krishna/project/HouZing/HouZingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.project.HouZing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouZingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
